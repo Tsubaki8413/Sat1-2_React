644822ffd7537230ef0b211fcf63e1ddfefc56d3
validName <>
2025-06-28 10:15:01 +0900

    20250628

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 function App() {
     const [inputText, setInputText] = useState("");
-    const [log, setLog] = useState("");
 
     return(
         <div>
