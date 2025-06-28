3bf7582b556464f15a53b73cb153f4eb94e8ad55
validName <>
2025-06-28 10:14:16 +0900

    20250628

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import React, { useState, useEffect } from "react";
 
 function App() {
     const [inputText, setInputText] = useState("");
-    const [log, setLog] = useState("");
 
     return(
         <div>
             <h1>フォームアプリ</h1>
 
-            <input
-              type="text"
-              placeholder="入力してください"
-              value={inputText}
-              onChange={(e) => setInputText(e.target.value)}
-            />
+            名前：<input
+                type="text"
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+            /><br/>
+            メール：<input
+                type="mail"
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+            /><br/>
+            コメント：<textarea
+                cols="30" rows="10"
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+            ></textarea>
             
-            <p>リアルタイム表示：{inputText}</p>
+
+            <h3>リアルタイム表示</h3>
+            <p>
+                名前：{inputText}<br/>
+                メール：<br/>
+                コメント：
+            </p>
         </div>
     );
 };
