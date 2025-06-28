e0b1f9c365c14f0b5e9940e5a80f3c486dff36d1
validName <>
2025-06-28 10:29:38 +0900

    20250628

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 function App() {
-    const [inputText, setInputText] = useState("");
+    const [name, setName] = useState("");
+    const [mail, setMail] = useState("");
+    const [comment, setComment] = useState("");
 
     return(
         <div>
@@ -9,26 +11,26 @@ function App() {
 
             名前：<input
                 type="text"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
             /><br/>
             メール：<input
                 type="mail"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
+                value={mail}
+                onChange={(e) => setMail(e.target.value)}
             /><br/>
             コメント：<textarea
                 cols="30" rows="10"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
             ></textarea>
             
 
             <h3>リアルタイム表示</h3>
             <p>
-                名前：{inputText}<br/>
-                メール：<br/>
-                コメント：
+                名前：{name}<br/>
+                メール：{mail}<br/>
+                コメント：{comment}
             </p>
         </div>
     );
